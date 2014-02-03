0fea527274244c2e8a93d6bdc8bc2378ac447814
Riku Walve <riku.walve@gmail.com>
2014-02-03 19:14:39 +0200

    Saarten piirto omaan luokkaan

--- Hashiwokakero/src/main/java/riku/hashiwokakero/ui/PeliLauta.java
+++ Hashiwokakero/src/main/java/riku/hashiwokakero/ui/PeliLauta.java
@@ -3,36 +3,19 @@ package riku.hashiwokakero.ui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Point;
+
 import javax.swing.JPanel;
 
 import riku.hashiwokakero.logiikka.Peli;
-import riku.hashiwokakero.logiikka.Saari;
 
 public class PeliLauta extends JPanel {
-    private Peli peli;
-    
     private Sillat sillasto;
+    private Saaret saaristo;    
     private Rakentaja rakentaja;
     
-    private static final Color[] VARIT = {
-        new Color(0, 0, 0),
-        
-        new Color(70, 137, 102),
-        new Color(255, 240, 165),
-        new Color(255, 176, 59),
-        new Color(182, 73, 38)
-    };
-    
-    private Color puolitaAlfa(Color c) {
-        return new Color(c.getRed(), c.getGreen(), c.getGreen(),
-                c.getAlpha() / 2);
-    }
-    
     public PeliLauta(Peli peli) {
-        this.peli = peli;
-        
         sillasto = new Sillat(peli.getSillat());
+        saaristo = new Saaret(peli.getSaaret());
         
         rakentaja = new Rakentaja(peli);
         addMouseListener(rakentaja);
@@ -49,14 +32,6 @@ public class PeliLauta extends JPanel {
         
         sillasto.piirra(g2);
         rakentaja.piirra(g2);
-        
-        for (Saari saari : peli.getSaaret().getSaaret()) {
-            g2.setColor(puolitaAlfa(VARIT[saari.getSillat()]));
-            g2.fillRect(saari.x - 24, saari.y - 24, 48, 48);
-            
-            g2.setColor(VARIT[saari.vaaditutSillat]);
-            g2.fillRect(saari.x - 18, saari.y - 18, 36, 36);
-        }
+        saaristo.piirra(g2);
     }
 }
-
---
+++ Hashiwokakero/src/main/java/riku/hashiwokakero/ui/Saaret.java
@@ -0,0 +1,47 @@
+package riku.hashiwokakero.ui;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import riku.hashiwokakero.logiikka.Saari;
+import riku.hashiwokakero.logiikka.SaariKartta;
+
+public class Saaret {
+    private SaariKartta saaret;
+    
+    public Saaret(SaariKartta saaret) {
+        this.saaret = saaret;
+    }
+    
+    private static final Color[] VARIT = {
+        new Color(0, 0, 0),
+        
+        new Color(70, 137, 102),
+        new Color(255, 240, 165),
+        new Color(255, 176, 59),
+        new Color(182, 73, 38),
+        
+        new Color(255, 255, 255),
+        new Color(255, 255, 255),
+        new Color(255, 255, 255)
+    };
+    
+    private Color puolitaAlfa(Color c) {
+        return new Color(c.getRed(), c.getGreen(), c.getGreen(),
+                c.getAlpha() / 2);
+    }
+    
+    public void piirra(Graphics2D g2) {        
+        for (Saari saari : saaret.getSaaret()) {
+            int keskix = (800 / 2) + (saari.x * 36) - (36 / 2);
+            int keskiy = (600 / 2) + (saari.y * 36) - (36 / 2);
+            
+            g2.setColor(puolitaAlfa(VARIT[saari.getSillat()]));
+            g2.fillRect(keskix - 6, keskiy - 6, 48, 48);
+            
+            g2.setColor(VARIT[saari.vaaditutSillat]);
+            g2.fillRect(keskix, keskiy, 36, 36);
+        }
+    }
+    
+}
