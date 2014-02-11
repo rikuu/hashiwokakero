bf2c09c5066723eb325083c48bc0027c0a4da3d6
Riku Walve <riku.walve@gmail.com>
2014-02-11 22:24:10 +0200

    Korjaa juttuja generaattorissa

--- Hashiwokakero/src/main/java/riku/hashiwokakero/logiikka/Generaattori.java
+++ Hashiwokakero/src/main/java/riku/hashiwokakero/logiikka/Generaattori.java
@@ -1,5 +1,6 @@
 package riku.hashiwokakero.logiikka;
 
+import java.awt.Point;
 import riku.hashiwokakero.domain.Saari;
 
 /**
@@ -8,12 +9,35 @@ import riku.hashiwokakero.domain.Saari;
 public class Generaattori {
     private SaariKartta saaret;
     
+    private SiltaKartta ratkaistu;
+    
+    // Pitää siirtää pois täältä, koskee pelkästään 800x600 ikkunaa
+    private final static int reunax0 = -10, reunay0 = -8;
+    private final static int reunax1 = 10, reunay1 = 8;
+    
     public Generaattori(SaariKartta saaret) {
         this.saaret = saaret;
+        
+        ratkaistu = new SiltaKartta();
     }
     
-    private int arvoEtaisyys() {
-        return (Math.random() < 0.5) ? -2 : 2;
+    private Point arvoPaikka(Saari pohja) {
+        Point paikka = new Point(pohja.x, pohja.y);
+
+        int p = (Math.random() <= 0.5) ? -2 : 2;
+        if (Math.random() <= 0.5) {
+            paikka.x += p;
+        } else {
+            paikka.y += p;
+        }
+        
+        // Huono, mahoton pelata, arvo uus
+        if ((paikka.x <= reunax0) || (paikka.x >= reunax1) ||
+            (paikka.y <= reunay0) || (paikka.y >= reunay1)) {
+                return arvoPaikka(pohja);
+        }
+        
+        return paikka;
     }
     
     private int arvoSiltojenMaara() {
@@ -32,38 +56,49 @@ public class Generaattori {
         return saaret.getSaaret().get(i);
     }
     
-    /**
-     * Lisää uuden saaren SaariKarttaan
-     */
-    public void uusiSaari() {
-        if (saaret.getSaaret().isEmpty()) {
-            saaret.lisaa(new Saari(0, 0, 0));
-            return;
+    private void lisaaSiltoja(Saari uusiSaari) {
+        for (Saari s : saaret.getSaaret()) {
+            if (uusiSaari.getVaaditutSillat() >= 4) {
+                return;
+            }
+            
+            if (saaret.saariaValissa(s, uusiSaari) ||
+                    ((s.x != uusiSaari.x) && (s.y != uusiSaari.y))) {
+                continue;
+            }
+            
+            int siltoja = arvoSiltojenMaara();
+            if (((s.getVaaditutSillat() + siltoja) <= 4)
+                    /*&& (Math.random() <= 0.4)*/) {
+                s.vaadiLisaa(siltoja);
+                uusiSaari.vaadiLisaa(siltoja);
+            }
         }
-        
-        int px = 0, py = 0;
-        if (Math.random() > 0.5) {
-            px = arvoEtaisyys();
-        } else {
-            py = arvoEtaisyys();
+    }
+    
+    /**
+     * Generoi uuden saaren
+     * @return saari
+     */
+    public Saari uusiSaari() {
+        if (saaret.getSaaret().isEmpty()) {
+            return new Saari(0, 0, 0);
         }
         
         int siltoja = arvoSiltojenMaara();
-        
         Saari pohja = valitsePohja(siltoja);
-        px += pohja.x;
-        py += pohja.y;
+        Point paikka = arvoPaikka(pohja);
+        Saari uusiSaari = new Saari(paikka.x, paikka.y, siltoja);
         
-        Saari uusSaari = new Saari(px, py, siltoja);
-        
-        if (saaret.getSaaret().contains(uusSaari) ||
-                saaret.saariaValissa(pohja, uusSaari)) {
-            uusiSaari();
-            return;
+        if (saaret.getSaaret().contains(uusiSaari) ||
+                saaret.saariaValissa(pohja, uusiSaari)) {
+            return uusiSaari();
         }
         
         pohja.vaadiLisaa(siltoja);
         
-        saaret.lisaa(uusSaari);
+        //lisaaSiltoja(uusiSaari);
+        
+        return uusiSaari;
     }
 }
