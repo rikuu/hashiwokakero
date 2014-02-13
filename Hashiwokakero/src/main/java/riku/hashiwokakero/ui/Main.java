c0a57790f157d5bfe30c17b5dbb15e0a166df52a
Riku Walve <riku.walve@gmail.com>
2014-02-13 19:37:22 +0200

    Mainin ei edes tarvii extendaa JFramee jos on selkeempi

--- Hashiwokakero/src/main/java/riku/hashiwokakero/ui/Main.java
+++ Hashiwokakero/src/main/java/riku/hashiwokakero/ui/Main.java
@@ -1,45 +1,52 @@
 package riku.hashiwokakero.ui;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import riku.hashiwokakero.logiikka.Peli;
 
 /**
  * Pelkästään luo ikkunan pelille.
  */
-public class Main extends JFrame implements Peli.RatkaisuTapahtuma {
+public class Main implements Runnable, Peli.RatkaisuTapahtuma {
+    private JFrame frame;
     private PeliLauta lauta;
     
     public Main() {
         lauta = null;
+    }
+    
+    @Override
+    public void run() {
+        frame = new JFrame("Hashiwokakero");
         
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(Util.resx, Util.resy);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
+       
         // On tää jossain mielessä tässä vaiheessa ratkaistu
         peliRatkaistu();
-        
-        setTitle("Hashiwokakero");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(Util.resx, Util.resy);
-        setLocationRelativeTo(null);
-        setVisible(true);
-        setResizable(false);
     }
     
     @Override
     public void peliRatkaistu() {
         if (lauta != null)
-            getContentPane().remove(lauta);
+            frame.getContentPane().remove(lauta);
         
         Peli peli = new Peli(10);
         peli.setTapahtuma(this);
         
         lauta = new PeliLauta(peli);
-        add(lauta);
+        frame.add(lauta);
         
-        getContentPane().invalidate();
-        getContentPane().validate();
+        frame.getContentPane().invalidate();
+        frame.getContentPane().validate();
     }
     
     public static void main(String[] args) {
-        new Main();
+        Main main = new Main();
+        SwingUtilities.invokeLater(main);
     }
 }
